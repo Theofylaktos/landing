6a6a86639d761a637bef02db1b1b636e50461007
theofylaktos <theofylaktos.a@gmail.com>
2020-01-31 23:05:29 +0200

    Updates

---
+++ precache-manifest.7fa63d202d1db9ae4d8abda5ea0e0d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097b586b468f9fe5a422fdf81c9a8efa",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "ec23b5d00d7c032f4f66",
+    "url": "/landing/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7c3635630f71287a3d33",
+    "url": "/landing/static/css/main.cd25fce6.chunk.css"
+  },
+  {
+    "revision": "ec23b5d00d7c032f4f66",
+    "url": "/landing/static/js/2.ee0fef95.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/landing/static/js/2.ee0fef95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3635630f71287a3d33",
+    "url": "/landing/static/js/main.ff762a37.chunk.js"
+  },
+  {
+    "revision": "e360e12435fa3906baaf",
+    "url": "/landing/static/js/runtime-main.8e076927.js"
+  }
+]);
